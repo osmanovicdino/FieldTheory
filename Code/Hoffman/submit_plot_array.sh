e6f8b19a8cd2163017ff5a5a874527cabc69055e
osmanovicdino <osmanovic.dino@gmail.com>
2022-04-07 15:59:42 -0700

    minor update

--- Code/Hoffman/submit_plot_array.sh
+++ Code/Hoffman/submit_plot_array.sh
@@ -31,7 +31,7 @@ module load ffmpeg
 dirwemake="chemistry${SGE_TASK_ID}"
 cd /u/scratch/d/dinoo/FieldTheory2/${dirwemake}
 export OMP_NUM_THREADS=1
-for file in *.csv; do python3 ~/FieldTheory/Code/Plotting/PlotDirectory.py "$file"; done
+for file in field0*.csv; do python3 ~/FieldTheory/Code/Plotting/PlotDirectory.py "$file"; done
 ffmpeg -pattern_type glob -i '*.png' -s 1920x1080 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" test.mp4
 # echo job info on joblog:
 echo "Job $JOB_ID ended on:   " `hostname -s`
