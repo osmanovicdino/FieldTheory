7f0f4c0c598ffd70e4857c3dff88ac93cf40c91e
osmanovicdino <osmanovic.dino@gmail.com>
2023-06-17 07:48:44 -0700

    minor update

--- Code/Hoffman/submit_plot_array.sh
+++ Code/Hoffman/submit_plot_array.sh
@@ -31,11 +31,11 @@ module load ffmpeg
 dirwemake="chemistry${SGE_TASK_ID}"
 cd /u/scratch/d/dinoo/Waves/${dirwemake}
 export OMP_NUM_THREADS=1
-num=`ls field0*.csv | wc -l`
+num=`ls field1*.csv | wc -l`
 if test $num -ge 100;
 then
     for file in field0*.csv; do python3 ~/FieldTheory/Code/Plotting/PlotDirectory.py "$file"; done
-    ffmpeg -pattern_type glob -i '*.png' -s 1920x1080 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" test.mp4
+    ffmpeg -pattern_type glob -i 'field1*.png' -s 1920x1080 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" test2.mp4
 fi
 # echo job info on joblog:
 echo "Job $JOB_ID ended on:   " `hostname -s`
