e1781d10698e2eda259818133ac49c277efaf3a5
osmanovicdino <osmanovic.dino@gmail.com>
2023-07-19 08:31:43 -0700

    minor update

--- Code/Hoffman/submit_array_job11.sh
+++ Code/Hoffman/submit_array_job11.sh
@@ -8,7 +8,7 @@
 #$ -l h_rt=5:00:00,h_data=2G
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -t 1-150:1
+#$ -t 1-90:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
--- Code/Hoffman/submit_plot_array.sh
+++ Code/Hoffman/submit_plot_array.sh
@@ -8,7 +8,7 @@
 #$ -l h_rt=3:00:00,h_data=8G
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -t 1-150:1
+#$ -t 1-90:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
