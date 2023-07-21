5af685b7926e6aa1196d3828c8cc57520e72b919
osmanovicdino <osmanovic.dino@gmail.com>
2023-07-21 15:10:18 -0700

    minor update

--- Code/Hoffman/submit_array_job11.sh
+++ Code/Hoffman/submit_array_job11.sh
@@ -5,10 +5,10 @@
 #$ -o joblog.$JOB_ID
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=5:00:00,h_data=2G
+#$ -l h_rt=3:00:00,h_data=2G
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -t 1-90:1
+#$ -t 1-110:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
@@ -35,10 +35,10 @@ else
    echo "did not read file correctly"
 fi
 dirwemake="chemistry${SGE_TASK_ID}"
-ftdir="WavesPhaseDiagram"
+ftdir="WavesPhaseDiagram2"
 mkdir /u/scratch/d/dinoo/${ftdir}/${dirwemake}
 cp ~/FieldTheory/Code/mainFractionalDiffusionNormalChem.cpp /u/scratch/d/dinoo/${ftdir}/${dirwemake}
-cp ~/FieldTheory/Code/InitialConditions/Cond8/${wt} /u/scratch/d/dinoo/${ftdir}/${dirwemake}/res.csv
+cp ~/FieldTheory/Code/InitialConditions/Cond9/${wt} /u/scratch/d/dinoo/${ftdir}/${dirwemake}/res.csv
 g++ ~/FieldTheory/Code/mainFractionalDiffusionNormalChem.cpp -lm -lfftw3 -L/usr/local/lib/lfftw3.a -std=c++17 -o /u/scratch/d/dinoo/${ftdir}/${dirwemake}/angron
 cd /u/scratch/d/dinoo/${ftdir}/${dirwemake}
 ./angron 'res.csv' > log
