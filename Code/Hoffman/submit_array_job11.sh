0321724379c6ca518a90c67e6c54465322a71abb
osmanovicdino <osmanovic.dino@gmail.com>
2023-08-17 14:35:53 -0700

    minor update

--- Code/Hoffman/submit_array_job11.sh
+++ Code/Hoffman/submit_array_job11.sh
@@ -8,7 +8,7 @@
 #$ -l h_rt=4:00:00,h_data=2G
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -t 1-120:1
+#$ -t 1-126:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
