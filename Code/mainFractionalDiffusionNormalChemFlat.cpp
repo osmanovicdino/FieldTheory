bed26aa7e9b360591749d7c02b84861585872fb4
osmanovicdino <osmanovic.dino@gmail.com>
2024-04-25 12:22:17 -0700

    minor update

--- Code/mainFractionalDiffusionNormalChemFlat.cpp
+++ Code/mainFractionalDiffusionNormalChemFlat.cpp
@@ -395,7 +395,7 @@ int main(int argc, char **argv)
     cout << "all fields set" << endl;
 
     // auto start = std::chrono::high_resolution_clock::now();
-    int runtime = 200000;
+    int runtime = 20000000;
     int every = 100;
 
     int tf = ceil((double)runtime / (double)every);
@@ -412,7 +412,7 @@ int main(int argc, char **argv)
     for (int i = 0; i < runtime; i++)
     {
 
-        if (i % every == 0 && i > 0 )
+        if (i % every == 0 && i > 10000000)
         {
             // stringstream strep1;
             // stringstream strep2;
