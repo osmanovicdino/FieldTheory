8d78418bc6e7ac2157cb4b1ff398a0f31acbd18d
osmanovicdino <osmanovic.dino@gmail.com>
2024-04-25 16:44:34 -0700

    minor update

--- Code/Hoffman/submit_array_job19.sh
+++ Code/Hoffman/submit_array_job19.sh
@@ -5,7 +5,7 @@
 #$ -o joblog.$JOB_ID
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=1:00:00,h_data=2G
+#$ -l h_rt=4:00:00,h_data=2G
 ## Modify the parallel environment
 ## and the number of cores as needed:
 #$ -t 1-64:1
