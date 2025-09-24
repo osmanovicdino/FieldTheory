772b13bba7fb9ee53c4d8b10db207e7e1252dadc
osmanovicdino <osmanovic.dino@gmail.com>
2025-09-24 15:02:28 -0700

    minor update

--- Code/Hoffman/submit_array_job_CLGE.sh
+++ Code/Hoffman/submit_array_job_CLGE.sh
@@ -35,7 +35,7 @@ else
    echo "did not read file correctly"
 fi
 dirwemake="chemistry${SGE_TASK_ID}"
-ftdir="CLGE3"
+ftdir="CLGE4"
 mkdir /u/scratch/d/dinoo/${ftdir}/${dirwemake}
 cp ~/FieldTheory/Code/mainCGLEVN.cpp /u/scratch/d/dinoo/${ftdir}/${dirwemake}
 g++ ~/FieldTheory/Code/mainCGLEVN.cpp -lm -lfftw3 -L/usr/local/lib/lfftw3.a -std=c++17 -o /u/scratch/d/dinoo/${ftdir}/${dirwemake}/angron
