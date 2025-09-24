c5f10724dc8b8a400667aa43b701ba98cf6bf448
osmanovicdino <osmanovic.dino@gmail.com>
2025-09-24 15:03:04 -0700

    minor update

--- Code/Hoffman/submit_plot_array.sh
+++ Code/Hoffman/submit_plot_array.sh
@@ -29,7 +29,7 @@ module load ffmpeg/5.0.1
 ##/usr/bin/time -v hostname
 
 dirwemake="chemistry${SGE_TASK_ID}"
-cd /u/scratch/d/dinoo/CLGE3/${dirwemake}
+cd /u/scratch/d/dinoo/CLGE4/${dirwemake}
 export OMP_NUM_THREADS=1
 num=`ls res*.csv | wc -l`
 if test $num -ge 1;
