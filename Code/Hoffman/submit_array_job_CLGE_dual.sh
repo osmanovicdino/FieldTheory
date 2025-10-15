a0fcf2b4f04085d0058bbf7f169e725adb81353a
osmanovicdino <osmanovic.dino@gmail.com>
2025-10-15 15:30:54 -0700

    minor update

--- Code/Hoffman/submit_array_job_CLGE_dual.sh
+++ Code/Hoffman/submit_array_job_CLGE_dual.sh
@@ -8,7 +8,7 @@
 #$ -l h_rt=6:00:00,h_data=2G
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -t 1-540:1
+#$ -t 301-540:1
 
 # echo job info on joblog:
 echo "Job $JOB_ID started on:   " `hostname -s`
